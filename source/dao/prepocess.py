9a99e1b2fc878fb6372c6d8d0d9e21e7441df5e8
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2026-03-26 10:14:31 +0700

    chore(requirements): add requirements

--- source/dao/prepocess.py
+++ source/dao/prepocess.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+import numpy as np
 
 BASE_DIR = Path(__file__).parents[0].resolve()
 DATA_DIR = BASE_DIR / "data"
 
-def data_load():
+def preprocess(data_path: str):
+    pass
+
+def split_dataset():
     pass
