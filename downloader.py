77de81fd222a60b7ff8011f029c5b9c602978883
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2026-03-26 09:20:58 +0700

    logic(downloader): change abs path to non-abs path

--- downloader.py
+++ downloader.py
@@ -2,13 +2,17 @@
 
 import kagglehub
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parents[0].resolve()
+DATA_DIR = BASE_DIR / "data"
 
 kagglehub.login()
 
-if not os.path.exist("data"):
-    os.makedirs("data")
+if not os.path.exist(DATA_DIR):
+    os.makedirs(DATA_DIR)
 
-path = kagglehub.dataset_download("Cornell-University/arxiv", output_dir="data")
+path = kagglehub.dataset_download("Cornell-University/arxiv", output_dir=DATA_DIR)
 
 print("Dataset downloaded successfully")
 print(f"Dataset located at: {path}")
---
+++ source/dao/prepocess.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parents[0].resolve()
+DATA_DIR = BASE_DIR / "data"
+
+def data_load():
+    pass
