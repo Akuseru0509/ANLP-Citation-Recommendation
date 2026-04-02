e3307840575963eee6750201168a705690859641
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2026-04-02 10:20:16 +0700

    feat(scraper): add scraper prototype

--- downloader.py
+++ downloader.py
@@ -5,14 +5,14 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parents[0].resolve()
-DATA_DIR = BASE_DIR / "data"
+DATA_DIR = BASE_DIR / "data" / "arxiv"
 
 kagglehub.login()
 
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-path = kagglehub.dataset_download("Cornell-University/arxiv", output_dir=DATA_DIR)
+path = kagglehub.dataset_download("Cornell-University/arxiv", output_dir=DATA_DIR, force_download=True)
 
 print("Dataset downloaded successfully")
 print(f"Dataset located at: {path}")
